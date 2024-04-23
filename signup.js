afa945815b973311bd05aa3f449103b971f91fa5
Ademola Elisha <ademolaelishaoluwapelumi@gmail.com>
2024-04-23 13:28:50 +0100

    commit ten

---
+++ signup.js
@@ -0,0 +1,17 @@
+const Signup = () => {
+    let inputone = document.getElementById('firstname')
+    let inputtwo = document.getElementById('lastname')
+    let inputthree = document.getElementById('day')
+    let inputfour = document.getElementById('month')
+    let inputfive = document.getElementById('year')
+    let phone = document.getElementById('phonenumber')
+    let inputsix = document.getElementById('email')
+    let inputseven = document.getElementById('password')
+    let inputnine = document.getElementById('terms')
+
+    if (inputone.value == '' || inputtwo.value == '' || inputthree.value == '' || inputfour.value == '' || inputfive.value == '' || phone.value == '' || inputsix.value == '' || inputseven.value == '' || inputnine.checked == ''  ) {
+        alert('haba')
+    } else if (phone) {
+        
+    }
+}
